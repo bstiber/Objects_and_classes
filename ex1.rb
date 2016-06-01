26219cc0f2e52e7f65d7ed4fb902b936ca0b27bf
Bill Stiber <billstiber@gmail.com>
2016-05-31 20:16:33 -0400

    add updates to file

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+# Instantiate a new instance of the String class and assign the newly instantiated instance
+# to the variable string.
+
+string = String.new
+
+# Instantiate a new instance of the Array class and assign the newly instantiated instance to the
+# variable array.
+
+array = Array.new
